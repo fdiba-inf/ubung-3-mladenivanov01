c97148e26ad2f14068b86863bbb4d61f62d84e15
mladenivanov01 <mladenivanov01@gmail.com>
2021-01-07 16:40:24 +0000

    ..

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -3,35 +3,48 @@ package exercise3;
 import java.util.Scanner;
 
 public class NumberCounter {
-  public static void main(String[]args) {
-    Scanner input  =  new Scanner(System.in);
-    int posCount=0;
-    int negCount =0;
-    double sum=0.0;
-    double  avarage=0.0;
-
-    int count=0;
-    int number =input.nextInt();
-    while (number  != 0) {
-      if (number> 0){
-      {posCount++;}
-      }else{
-      {negCount++;}
-      }
-      
-      sum+=number;
-      number=input.nextInt();
+  public static void main (String[] args) {
+  
+  Scanner input = new Scanner(System.in);
 
 
-    }
-      avarage= sum /(posCount+negCount);
+  int positiv = 0;
 
-    System.out.println("Positive numbers: "+ posCount);
-    System.out.println("Negative numbers: "+ negCount);
-    System.out.println("Sum: "+ sum);
-    System.out.println("Avarage: " +  avarage);  
-  }
+  int negativ= 0;
+
+  double sum = 0.0;
+
+  double avarage = 0.0;
+
+  int count=0;
+  
+ int number = input.nextInt();
+
+  while ( number != 0) {
     
+    if (number > 0){
+      positiv++;
+    }else{
+      negativ++;
+      }
+
+    count++;
+    sum = sum + number;
+    
+    number=input.nextInt();
+  
+   
+  }
+
+  avarage = (sum)/count;
+
+  System.out.println("Positive numbers: "+ positiv);
+  System.out.println("Negative numbers: "+ negativ);
+  System.out.println("Sum: " + sum);
+  System.out.println("Average: " + avarage);
+  
+
 
   }
+}
 
